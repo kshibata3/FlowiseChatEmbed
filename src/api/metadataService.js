3db0ce7b5d183c857b647e8a80d27dfaa7538a01
kshibata3 <150947812+kshibata3@users.noreply.github.com>
2024-04-19 13:53:48 -0700

    update sourcebubble

--- src/api/metadataService.js
+++ src/api/metadataService.js
@@ -1,6 +1,6 @@
 const fetchMetadata = async (url) => {
   try {
-    const apiKey = '500ea4641f1b0acfc3e91eed823c3be2';  // Replace 'YOUR_API_KEY' with your actual LinkPreview API key
+    const apiKey = '500ea4641f1b0acfc3e91eed823c3be2'; // Replace 'YOUR_API_KEY' with your actual LinkPreview API key
     const response = await fetch(`https://api.linkpreview.net/?key=${apiKey}&q=${encodeURIComponent(url)}`);
 
     if (!response.ok) {
@@ -11,13 +11,13 @@ const fetchMetadata = async (url) => {
     // Check if data contains the necessary fields, assume some might be missing
     return {
       title: data.title || 'No title available',
-      image: data.image || 'default_image.png',  // Specify a default image path if none is found
+      image: data.image || 'default_image.png', // Specify a default image path if none is found
       description: data.description || 'No description available',
-      url: data.url || url,  // Fall back to the original URL if none is provided
+      url: data.url || url, // Fall back to the original URL if none is provided
     };
   } catch (error) {
     console.error('Error fetching metadata:', error);
-    throw error;  // Re-throw to handle it in the calling code
+    throw error; // Re-throw to handle it in the calling code
   }
 };
 
